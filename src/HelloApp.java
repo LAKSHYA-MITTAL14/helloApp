f8052dd93da4da48c5ed5866272f31fa9a6b65a5
LAKSHYA-MITTAL14 <lakshyamittal14@gmail.com>
2026-03-26 13:48:26 +0530

    hello/UC-4

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,5 +1,14 @@
-public class HelloApp {
-    public static void main(String[] args) {
-        
+public class main {
+    public static void main (String args[]) {
+        StringBuilder names= new StringBuilder();
+        for(int i=0;i<args.length;i++){
+           names.append(args[i]);
+           if(i<args.length-1){
+            names.append(",");
+           }  
+
+        }
+        String name=names.toString();
+        System.out.println("Hello, " + name + "!");
     }
 }
