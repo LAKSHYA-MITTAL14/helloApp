d3b6fa8ef18514892e6fc9f8d5b9f2e8b4ef6e19
LAKSHYA-MITTAL14 <lakshyamittal14@gmail.com>
2026-03-26 14:03:27 +0530

    hello/UC-6

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,13 +1,24 @@
 public class HelloApp {
     public static void main(String[] args) {
-        
-        String name = "World";
 
         
-        if (args.length > 0) {
-            name = args[0]; // Use the provided name
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
         }
 
-        System.out.println("Hello, " + name + "!");
+        
+        StringBuilder nameBuilder = new StringBuilder();
+
+        
+        for (String name : args) {
+            nameBuilder.append(name).append(", ");
+        }
+
+        
+        String result = nameBuilder.substring(0, nameBuilder.length() - 2);
+
+        
+        System.out.println("Hello, " + result + "!");
     }
 }
